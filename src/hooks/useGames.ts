49800842317a88d983aed278a79d0d063d057f8b
Richard Wells <richard.wells@virginmediao2.co.uk>
2023-04-10 13:49:50 +0100

    adds custom hook for games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { CanceledError } from "axios";
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<GameResponse>("/games", { signal: controller.signal })
+      .then((response) => {
+        setGames(response.data.results);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+    return () => controller.abort();
+  }, []);
+  return { games, error };
+};
+
+export default useGames;
