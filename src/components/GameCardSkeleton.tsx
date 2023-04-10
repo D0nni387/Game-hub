1502a49ce98b640be98c0feb57b8df995d67be9b
Richard Wells <richard.wells@virginmediao2.co.uk>
2023-04-10 16:46:02 +0100

    removed duplicate styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,12 +3,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 export const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton
-        height="200px"
-        width="300px"
-        borderRadius={10}
-        overflow="hidden"
-      />
+      <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
